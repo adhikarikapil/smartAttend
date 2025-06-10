2d8f90aa4ad3ebd74926ab5f54410b4a82fcb85c
Kapil Adhikari <kapiladhikari2060@gmail.com>
2025-06-10 13:08:23 +0545

    extentions: WebSocket Extention

--- backend/app/extentions.py
+++ backend/app/extentions.py
@@ -2,11 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_socketio import SocketIO
 
 db = SQLAlchemy()
 jwt = JWTManager()
 cors = CORS()
 migrate = Migrate()
+socketio = SocketIO(cors_allowed_origins='*')
 
 # Setup JWT blacklist 
 @jwt.token_in_blocklist_loader
